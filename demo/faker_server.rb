6ac88e1d76178ff16d7ff811194ebfcacdb365c1
rick <technoweenie@gmail.com>
2009-12-19 13:29:48 -0800

    implement users/show in the faker server

--- demo/faker_server.rb
+++ demo/faker_server.rb
@@ -2,11 +2,12 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'sinatra'
 require 'twitter_server'
 require 'faker'
+require 'active_support'
 
 TwitterServer.xml_renderer = TwitterServer::Renderer::NokogiriRenderer
 
-$user   = {:id => 1, :screen_name => Faker::Name.name}
-$friend = {:id => 2, :screen_name => Faker::Name.name}
+$user   = {:id => 1, :name => Faker::Name.name}
+$friend = {:id => 2, :name => Faker::Name.name}
 $statuses = {
   $user => [
     {:id => 1, :text => 'oh hai', :source => 'api'},
@@ -17,9 +18,14 @@ $statuses = {
     ]
 }
 
+$users = [$user, $friend]
+$users.each { |u| u[:screen_name] = u[:name].underscore }
 $statuses.each do |user, statuses|
   statuses.each { |st| st[:user] = user }
 end
+
+$user_id_index     = $users.inject({}) { |memo, user| memo.update(user[:id] => user) }
+$screen_name_index = $users.inject({}) { |memo, user| memo.update(user[:screen_name] => user) }
 
 twitter_help
 
@@ -34,6 +40,14 @@ end
 twitter_statuses_user_timeline do |params|
   user     = params[:user_id] == '1' ? $user : $friend
   $statuses[user]
+end
+
+twitter_users_show do |params|
+  if user_id = (params[:id] || params[:user_id])
+    $user_id_index[user_id]
+  elsif params[:screen_name]
+    $screen_name_index[params[:screen_name]]
+  end
 end
 
 twitter_account_verify_credentials do |params|
